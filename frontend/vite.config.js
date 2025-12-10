fd087d8f7ea593cf269566af903bee397aa09396
Mayuushi <canenciawalter@gmail.com>
2025-12-10 16:02:26 +0800

    Adjusted

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/react-NotesCRUD',
   server: {
     port: 3000,
     proxy: {
@@ -13,5 +14,6 @@ export default defineConfig({
       }
     }
   }
+  
 })
 
